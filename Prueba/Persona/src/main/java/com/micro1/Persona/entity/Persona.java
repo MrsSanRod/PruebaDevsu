8fcad312348e494d1710958aec65dba5d7110698
MrsSanRod <mcsanrodriguez@gmail.com>
2024-06-26 10:23:42 -0500

    Update Persona.java

--- Prueba/Persona/src/main/java/com/micro1/Persona/entity/Persona.java
+++ Prueba/Persona/src/main/java/com/micro1/Persona/entity/Persona.java
@@ -1,7 +1,11 @@
 package com.micro1.Persona.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 public class Persona {
     @Id
@@ -13,62 +17,5 @@ public class Persona {
     private String identificacion;
     private String direccion;
     private String telefono;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    public String getIdentificacion() {
-        return identificacion;
-    }
-
-    public void setIdentificacion(String identificacion) {
-        this.identificacion = identificacion;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
 
 }
